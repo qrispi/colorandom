bf4e757984128cf1da040934027d5538f07864c6
Jason Kirchman <kirchman.jason@icloud.com>
2023-01-02 16:08:37 -0600

    feat: Create Palette class and add method to replace colors

--- src/classes.js
+++ src/classes.js
@@ -1,6 +1,25 @@
 class Color {
-    constructor(locked) {
+    constructor() {
         this.hex = getRandomHex();
-        this.locked = locked || false;
+        this.locked = false;
+    }
+
+    toggleLock() {
+        this.locked = !this.locked;
+    }
+}
+
+class Palette {
+    constructor() {
+        this.colors = [new Color(),new Color(),new Color(),new Color(),new Color()];
+        this.id = Date.now();
+    }
+    
+    replaceColors() {
+        for(var i = 0; i < this.colors.length; i++) {
+            if(!this.colors[i].locked) {
+                this.colors[i] = new Color();
+            }
+        }
     }
 }
