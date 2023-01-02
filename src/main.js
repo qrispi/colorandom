2d3fa8e5899add880fe49961e67dc40c716ea47b
Christopher Baum <christopherabaum@gmail.com>
2023-01-02 16:50:13 -0600

    feat: Finish the getRandomHex function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 function getRandomHex() {
     var characters = 'ABCDEF0123456789'.split('');
+    var hexCode = '#'
+    for (var i = 0; i < 6; i++) {
+        var randomNum = Math.floor(Math.random() * characters.length)
+        hexCode += characters[randomNum]
+    }
+    return hexCode
 }
