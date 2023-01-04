9a32ce9941d3965518f23cc6790b4d90b880c638
Trey Marshall <marshall.trey92@yahoo.com>
2023-01-03 19:53:44 -0600

    feat: Add function to connect color palette with HTML

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+var colorWidgetParent = document.querySelector('.color-widget-parent');
+var currentPalette = new Palette();
+
+
+window.addEventListener('load', reloadPalette());
+
 function getRandomHex() {
     var characters = 'ABCDEF0123456789'.split('');
     var hexCode = '#'
@@ -6,4 +12,26 @@ function getRandomHex() {
         hexCode += characters[randomNum]
     }
     return hexCode
+}
+
+function reloadPalette() {
+    // loop through current palette colors
+    // inside loop clear current HTML
+        // make new color widget
+        // append inner html
+    colorWidgetParent.innerHTML = ''
+    for (var i = 0; i < 5; i++) {
+        var imgString = 'Unlock.png'
+        if (currentPalette.colors[i].locked) {
+            imgString = 'Lock.png'
+        }
+        colorWidgetParent.innerHTML +=
+            `<article class="color-widget">
+                <div style="background-color:${currentPalette.colors[i].hex}" class="color-box"></div>
+                <div class="color-box-footer">
+                    <p>${currentPalette.colors[i].hex}</p>
+                    <img id="${i}" src="./assets/${imgString}">
+                </div>
+            </article>`           
+    }
 }
