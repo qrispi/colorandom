e9712580721cdecb8065afec9ffaecb1c78e0674
Christopher Baum <christopherabaum@gmail.com>
2023-01-06 10:10:04 -0600

    Store inverted color theme

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ var currentPalette = new Palette();
 var savedPalettes = [];
 var counter = -1;
 var fonts = ["'Barrio', cursive", "'Bungee Shade', cursive", "'Cabin Sketch', cursive", "'Monoton', cursive", "'Moo Lah Lah', cursive", "'Schoolbell', cursive"];
-var colors = ["#ef3550", "#f48fb1", "#7e57c2", "#2196f3", "#26c6da", "#43a047", "#00ff80", "#eeff41", "#f9a825", "#ff5722"];
+var colors = ["#ef3550", "#f48fb1", "#7e57c2", "#2196f3", "#26c6da", "#43a047", "#00ff80", "#e6d847", "#f9a825", "#ff5722"];
 var titleLetters = 'COLORANDOM'.split('');
 
 window.addEventListener('load', loadPage);
@@ -19,51 +19,22 @@ savedSection.addEventListener('click', deleteSavedPalette);
 function loadPage() {
     displayRandomTitleFonts();
     displayCurrentPalette();
-    var firstTimer = setInterval(printNum, 1000)
+    var timer = setInterval(animateTitleFonts, 1000)
 }
-
-function printNum() {
-    counter++
-    if (counter > 9) {
-        counter = 0
-    }
-    var spans = document.querySelectorAll('span')
-    // console.log(spans[counter])
-    console.log("span index font", spans[counter].dataset.fontIndex)
-    var currentFontIndex = spans[counter].dataset.fontIndex
-    var randomFont = getRandomFont(currentFontIndex)
-    var fontIndex = fonts.indexOf(randomFont)
-    console.log('new font index: ', fontIndex)
-    
-    spans[counter].outerHTML = `<span data-font-index="${fontIndex}" style="font-family: ${randomFont}; color: ${colors[counter]};">${titleLetters[counter]}</span>`
-}
-
-function getRandomFont(fontIndex) {
-    var newFonts = [...fonts]
-    console.log("font array copy", newFonts)
-    newFonts.splice(fontIndex, 1)
-    console.log("font array copy after splice", newFonts)
-    var randomFont = newFonts[Math.floor(Math.random() * newFonts.length)];
-    return randomFont
-}
-
-// function checkForRandomFont() {
-
-// }
 
 function displayCurrentPalette() {
     colorWidgetParent.innerHTML = '';
     for (var i = 0; i < 5; i++) {
-        var imgString = 'Unlock.png';
+        var imgString = 'Unlock-White.png';
         if (currentPalette.colors[i].locked) {
-            imgString = 'Lock.png';
+            imgString = 'Lock-White.png';
         }
         colorWidgetParent.innerHTML +=
             `<article class="color-widget">
                 <div data-index-number="${i}" style="background-color:${currentPalette.colors[i].hex}" class="color-box"></div>
                 <div class="color-box-footer">
-                    <p>${currentPalette.colors[i].hex}</p>
-                    <img  src="./assets/${imgString}">
+                    <p style="color: white; font-size: 3vmin;">${currentPalette.colors[i].hex}</p>
+                    <img src="./assets/${imgString}">
                 </div>
             </article>`           
     }
@@ -79,7 +50,7 @@ function displaySavedPalettes() {
             <div class="mini-color-box" style="background-color: ${savedPalettes[i].colors[2].hex}"></div>
             <div class="mini-color-box" style="background-color: ${savedPalettes[i].colors[3].hex}"></div>
             <div class="mini-color-box" style="background-color: ${savedPalettes[i].colors[4].hex}"></div>
-            <img src="./assets/Delete.png">
+            <img src="./assets/Delete-White.png">
         </article>`
     }
 }
@@ -91,15 +62,20 @@ function displayRandomTitleFonts() {
         var randomNum = Math.floor(Math.random() * fonts.length);
         header.innerHTML += `<span data-font-index="${randomNum}" style="font-family: ${fonts[randomNum]}; color: ${colors[i]};">${titleLetters[i]}</span>`
     }
-    // console.log(header.innerHTML)
 }
 
 function animateTitleFonts() {
-  num++
-  if (num > 10) {
-    num = 0
-  }
-  
+    counter++
+    if (counter > 9) {
+        counter = 0
+    }
+    var spans = document.querySelectorAll('span')
+    // console.log("span index font", spans[counter].dataset.fontIndex)
+    var currentFontIndex = spans[counter].dataset.fontIndex
+    var randomFont = getRandomFont(currentFontIndex)
+    var fontIndex = fonts.indexOf(randomFont)
+    // console.log('new font index: ', fontIndex)
+    spans[counter].outerHTML = `<span data-font-index="${fontIndex}" style="font-family: ${randomFont}; color: ${colors[counter]};">${titleLetters[counter]}</span>`
 }
 
 function changePaletteColors() {
@@ -143,3 +119,21 @@ function getRandomHex() {
     }
     return hexCode;
 }
+
+function getRandomFont(fontIndex) {
+    var newFonts = [...fonts]
+    // console.log("font array copy", newFonts)
+    newFonts.splice(fontIndex, 1)
+    // console.log("font array copy after splice", newFonts)
+    var randomFont = newFonts[Math.floor(Math.random() * newFonts.length)];
+    return randomFont
+}
+
+
+// Changed yellow letter color to be more readable
+
+// 
+
+// create 'party-mode' button that toggles between white and black themes
+// p text is not scaling or inheriting CSS due to not having a class
+// Seems like items without classes do not inherit certain properties
